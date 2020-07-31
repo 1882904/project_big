4aced5c7b974502d985561e2a2e9c49c84fa13ae
王锦 <1249505001@qq.com>
2020-07-31 13:18:56 +0800

    index页面完成

---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    getuserInfo ()
+
+})
+function getuserInfo () {
+    $.ajax ({
+        method:'GET',
+        url:'/my/userinfo',
+        headers:{
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success:function(res) {
+            console.log(res);
+           if(res.status!==0) {
+               console.log(res);
+            return layui.layer.msg('获取用户信息失败')
+           }
+           renderAvatar (res.data)
+
+        }
+    })
+}
+function renderAvatar (user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
+    if(user.user_pic!==null) {
+        $('.layui-nav-img').attr('src',user.user_pic)
+        .show()
+        $('.text-pig').hide()
+    }else {
+        $('.layui-nav-img').hide()
+        var one=name[0].toUpperCase()
+        $('.text-pig').html(one).show()
+        
+
+    }
+}
+var layer=layui.layer
+$('#btnLogout').on('click',function() {
+    layer.confirm('确定退出登录？', {icon: 3, title:'提示'}, function(index){
+        localStorage.removeItem('token')
+        location.href=('/login.html')
+        //do something
+        
+        layer.close(index);
+      });
+
+})
